702562dfd31a9183149583b7f9b5d7a7dbe72475
Anshu0105 <anshumanmishra0105@gmail.com>
2026-03-26 12:06:51 +0530

    Add Code for external Access PORT from RENDER and Update .gitignore file

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    server.listen(PORT, () => {
+    server.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
